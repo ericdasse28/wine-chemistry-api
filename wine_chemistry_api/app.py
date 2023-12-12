9dda72a73cc39ff1d3025857f44ebdf6d762c7e1
Eric Dasse <christ.gououi@gmail.com>
2023-12-12 12:38:05 +0100

    Update app.py

--- wine_chemistry_api/app.py
+++ wine_chemistry_api/app.py
@@ -3,10 +3,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/wine_quality/estimate")
+@app.route("/wine_quality/estimate", methods=["POST"])
 def predict_wine_quality():
     """Endpoint that predicts wine quality."""
 
     wine_features = request.json
 
     # Code here
+
+
+if __name__ == "__main__":
+    app.run()
